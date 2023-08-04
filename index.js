1ff8c8222d7886494c1a1dc8cc11cd7d9b607751
Ganeshdwivedi17 <139634932+Ganeshdwivedi17@users.noreply.github.com>
2023-08-04 11:11:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let timeout;
+$("#span1").click(function span1() {
+    $(".testimonial-image").attr("src", "images/abstraction_chat_2x.05c32ff8e884238e85beb0e339458f62.png");  
+     
+});
+
+$("#span2").click(function () {
+    $(".testimonial-image").attr("src", "images/abstraction_doc_2x.a22835a4f3e878497a2d3c4a99dea0c7.png" );     
+});
+
+$("#span3").click(function () {
+    $(".testimonial-image").attr("src", "images/abstraction_sheet_2x.ba7cdd8449efa44223408887bdac864c.png");     
+});
